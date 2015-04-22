8c23cae20c9e3c2f7cc78a9821b5579905fe4471
Eduardo Matos <eduardo.matos.silva@gmail.com>
2015-04-22 20:49:27 -0300

    add api endpoint

---
+++ authentication/permissions.py
@@ -0,0 +1,9 @@
+from rest_framework import permissions
+
+
+class IsAccountOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, account):
+        if request.user:
+            return request.user == account
+
+        return False
